13cdc9f8bca6d5e8d2a7755774518c246bb8331d
myleftshoe <dev.paulesammut@gmail.com>
2025-04-25 19:33:10 +1000

    .

--- src/routes/examples/programmaticcontrol.svelte
+++ src/routes/examples/programmaticcontrol.svelte
@@ -17,9 +17,11 @@
 	<div class="p-4">
 		<p>This sheet is controlled programatically. Use the <code>bind:this=&#123;bottomsheet&#125;</code> to expose the instance methods</p>
 		<br />
-		<li>showModal()</li>
-		<li>close()</li>
-		<li>snapTo(index)</li>
+		<ul class="list-inside list-disc">
+			<li>showModal()</li>
+			<li>close()</li>
+			<li>snapTo(index)</li>
+		</ul>
 	</div>
 	<div class="flex flex-col gap-4 p-8">
 		<button class="btn btn-sm preset-outlined-secondary-600-400" onclick={() => bottomsheet.snapTo(1)}>Snap</button>
