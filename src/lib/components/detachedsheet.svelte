26ef3a2ae947c86e7e9d69dd4b21d32bee6060c7
myleftshoe <dev.paulesammut@gmail.com>
2025-05-03 22:38:19 +1000

    fix: update border-radius in customStyle for detachedsheet component

--- src/lib/components/detachedsheet.svelte
+++ src/lib/components/detachedsheet.svelte
@@ -1,9 +1,9 @@
 <script>
 	import Bottomsheet from '$lib/components/bottomsheet.svelte'
 	let { open = $bindable(false), style = '', ...props } = $props()
-	const customStyle = 'width: calc(100% - 0.75rem); margin-bottom: 0.375rem; border-radius: var(--diaper-radius);'
+	const customStyle = 'width: calc(100% - 0.75rem); margin-bottom: 0.375rem; border-radius: 1.5rem;'
 </script>
 
-<Bottomsheet bind:open class="sheet" maxHeight="calc(93% - 0.25rem)" style="{customStyle} {style}" {...props}>
+<Bottomsheet bind:open maxHeight="calc(93% - 0.25rem)" style="{customStyle} {style}" {...props}>
 	{@render props.children?.()}
 </Bottomsheet>
