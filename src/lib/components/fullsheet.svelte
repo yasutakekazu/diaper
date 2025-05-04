78ad8a0db9a5ce2dce776f6e30d2cb0fa35ded4b
myleftshoe <dev.paulesammut@gmail.com>
2025-05-05 08:17:40 +1000

    fullsheet: padding-top: env(safe-area-inset-top);

--- src/lib/components/fullsheet.svelte
+++ src/lib/components/fullsheet.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Bottomsheet from '$lib/components/bottomsheet.svelte'
 	let { open = $bindable(false), style = '', ...props } = $props()
-	const customStyle = 'border-radius: 0;'
+	const customStyle = 'border-radius: 0; padding-top: env(safe-area-inset-top);'
 </script>
 
 <Bottomsheet bind:open class="sheet" maxHeight="100%" flat style="{customStyle} {style}" {...props}>
