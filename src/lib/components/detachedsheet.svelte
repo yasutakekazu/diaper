7b1b4dfca2c32c77e66b6afc3926cec7682adff7
myleftshoe <dev.paulesammut@gmail.com>
2025-05-05 08:26:38 +1000

    increased detached border-radius to 2rem to match iphone screen

--- src/lib/components/detachedsheet.svelte
+++ src/lib/components/detachedsheet.svelte
@@ -1,9 +1,9 @@
 <script>
 	import Bottomsheet from '$lib/components/bottomsheet.svelte'
 	let { open = $bindable(false), style = '', ...props } = $props()
-	const customStyle = 'width: calc(100% - 0.75rem); margin-bottom: 0.375rem; border-radius: 1.5rem;'
+	const customStyle = 'width: calc(100% - 0.75rem); margin-bottom: 0.375rem; border-radius: 2rem;'
 </script>
 
-<Bottomsheet bind:open maxHeight="calc(93% - 0.25rem)" style="{customStyle} {style}" {...props}>
+<Bottomsheet bind:open maxHeight="calc(93% - 0.375rem)" style="{customStyle} {style}" {...props}>
 	{@render props.children?.()}
 </Bottomsheet>
