45aeb9ece4f6711e6d234045939de8b5653172d7
myleftshoe <dev.paulesammut@gmail.com>
2025-05-05 23:02:30 +1000

    1.3.2 Merging into main

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,9 +1,9 @@
 import Bottomsheet from './bottomsheet.svelte'
-import Detailsheet from './detachedsheet.svelte'
+import Detachedsheet from './detachedsheet.svelte'
 import Fullsheet from './fullsheet.svelte'
 import Longsheet from './longsheet.svelte'
 
 import Renderroute from './renderroute.svelte'
 
 export default Bottomsheet
-export { Bottomsheet, Detailsheet, Fullsheet, Longsheet, Renderroute }
+export { Bottomsheet, Detachedsheet, Fullsheet, Longsheet, Renderroute }
