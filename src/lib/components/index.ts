004494554c4d3080d139bd4104228f34be1d201f
myleftshoe <dev.paulesammut@gmail.com>
2025-05-05 20:18:01 +1000

    updated exports

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,9 @@
 import Bottomsheet from './bottomsheet.svelte'
+import Detailsheet from './detachedsheet.svelte'
+import Fullsheet from './fullsheet.svelte'
+import Longsheet from './longsheet.svelte'
+
 import Renderroute from './renderroute.svelte'
 
 export default Bottomsheet
-export { Renderroute }
+export { Bottomsheet, Detailsheet, Fullsheet, Longsheet, Renderroute }
