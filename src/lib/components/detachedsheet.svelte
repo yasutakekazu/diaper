f3d4e0a544ff602933d94ed0d2b7522b07b77612
myleftshoe <dev.paulesammut@gmail.com>
2025-05-13 18:22:45 +1000

    export methods from detachedsheet and fullsheet

--- src/lib/components/detachedsheet.svelte
+++ src/lib/components/detachedsheet.svelte
@@ -3,9 +3,17 @@
 	import type { BottomsheetProps } from './types'
 
 	let { open = $bindable(false), style = '', ...props }: BottomsheetProps = $props()
+
 	const customStyle = 'width: calc(100% - 0.75rem); margin-bottom: calc(env(safe-area-inset-bottom) + 0.375rem); border-radius: 2rem;'
+
+	let bs: Bottomsheet
+
+	export const showModal = () => bs.showModal()
+	export const close = () => bs.close()
+	export const snapTo = (index: number) => bs.snapTo(index)
+	export const setIndex = (index: number) => bs.setIndex(index)
 </script>
 
-<Bottomsheet bind:open maxHeight="calc(93% - 0.375rem)" style="{customStyle} {style}" {...props}>
+<Bottomsheet bind:this={bs} bind:open maxHeight="calc(93% - 0.375rem)" style="{customStyle} {style}" {...props}>
 	{@render props.children?.()}
 </Bottomsheet>
